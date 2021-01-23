7e47f0ff7f6bbf952aeefc963c4bcb4a1b5b82a8
AliColak <alicolakk2014@gmail.com>
2021-01-23 19:02:43 +0200

    Add project files.

---
+++ DictionaryYapisi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DictionaryYapisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> teams = new MyDictionary<string, string>();
+
+            teams.Add("1", "Galatasaray");
+            teams.Add("2", "Fenerbahçe");
+
+            teams.Read();
+        }
+    }
+}
